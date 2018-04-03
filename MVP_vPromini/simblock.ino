8c5483fe1bb064f5155a37d716ab51be709737a8
evDres <andresastudilloo@gmail.com>
2018-04-03 19:06:53 -0300

    Update simblock.ino

--- MVP_vPromini/simblock.ino
+++ MVP_vPromini/simblock.ino
@@ -1,3 +1,5 @@
+// Agregar Serial number con comandos AT+CGSN o AT+CSN
+
 /***********************
  ****** Libraries ******
  ***********************/
@@ -9,18 +11,19 @@
  ****** Runtime Variables ******
  ******************************/
 
-#define baud_serial_to_SIM800L 9600 
+//#define baud_serial_to_SIM800L 9600 
+#define baud_serial0 9600 
 
 /***********************************
  ****** Serial Communications ******
  ***********************************/
+SoftwareSerial readerSerial(4,3); // Pro mini reader RX TX
  
-//SIM_serial.begin(baud_serial_to_SIM800L); //Software serial for SIM800L, wired to pins 7,8, handled by SoftwareSerial library
+    //SIM_serial.begin(baud_serial_to_SIM800L); //Software serial for SIM800L, wired to pins 7,8, handled by SoftwareSerial library
 GPRS gprs;                                //Software serial for SIM800L, wired to pins 7,8, handled by gprs_lite library
-//SoftwareSerial SIM_serial(8,7);     //SIM800L
-//SIM800L's serial can be handled by gprs_lite library
-//SoftwareSerial BTOBD_serial(2,3); //BT-OBD
-//Serial for BT-OBD changed to "hardware" serial (pins 0,1)
+    //SoftwareSerial SIM_serial(8,7);     //SIM800L
+    //SIM800L's serial can be handled by gprs_lite library
+    //Serial for BT-OBD changed to "hardware" serial (pins 0,1)
 
 
 /****************************
@@ -30,7 +33,8 @@ GPRS gprs;                                //Software serial for SIM800L, wired t
 char server[] = "REPLACE_WHEN_LOADING_TO_ARDUINO";     //Server's address
 int port = 00; //REPLACE WHEN LOADING TO ARDUINO
 //String data_to_send="";
-char data_to_send[300];
+
+char data_to_send[512];
 
 /*****************************************
  *****************************************
@@ -39,24 +43,38 @@ char data_to_send[300];
  *****************************************/
  
 void setup() {  
-  
+
+  Serial.begin(baud_serial0);
+  readerSerial.begin(baud_serial0);
+
   /********************************
    ****** Initialize SIM800L ******
    ******  & connect to APN  ******
    ********************************/
-   
-  while(0 != gprs.init()) delay(5000);
-  gprs.sendCmdAndWaitForResp("AT+CGATT?\r\n","OK",DEFAULT_TIMEOUT);
+  
+  while(0 != gprs.init()) delay(1000);
+  delay(5000);
+  if(0==gprs.sendCmdAndWaitForResp("AT+CFUN=1\r\n","OK",DEFAULT_TIMEOUT)){
+    Serial.println("CFUN=1");
+  }
+  delay(2000);
+  if(0==gprs.sendCmdAndWaitForResp("AT+CGATT?\r\n","OK",DEFAULT_TIMEOUT)){
+    Serial.println("CGATT OK");
+  }
   gprs.sendCmd("AT+CIPSTATUS\r\n");
-  delay(1000);
+  delay(2000);
   //REPLACE APN, USERNAME, AND PASSWORD
-  //gprs.sendCmd("AT+CSTT=\"WEB.TMOVIL.CL\",\"WEB\",\"WEB\"\r\n");                    //Movistar (Chile)
+  //gprs.sendCmd("AT+CSTT=\"WAP.TMOVIL.CL\",\"WAP\",\"WAP\"\r\n");                    //Movistar (Chile)
+  gprs.sendCmd("AT+CSTT=\"WEB.TMOVIL.CL\",\"WEB\",\"WEB\"\r\n");                    //Movistar (Chile)
   //gprs.sendCmd("AT+CSTT=\"IMOVIL.ENTELPCS.CL\",\"ENTELPCS\",\"ENTELPCS\"\r\n");   //entel 1 (Chile)
-  gprs.sendCmd("AT+CSTT=\"BAM.ENTELPCS.CL\",\"ENTELPCS\",\"ENTELPCS\"\r\n");      //entel 2 (Chile)
-  delay(1000);
+  //gprs.sendCmd("AT+CSTT=\"BAM.ENTELPCS.CL\",\"ENTELPCS\",\"ENTELPCS\"\r\n");      //entel 2 (Chile)
+  delay(3000);
   gprs.sendCmdAndWaitForResp("AT+CIICR\r\n","OK",DEFAULT_TIMEOUT);
-  gprs.sendCmd("AT+CIFSR\r\n");
   delay(1000);
+  gprs.sendCmd("AT+CIFSR\r\n");
+  delay(4000);
+  Serial.println("2G Initialized.");
+ 
   
 }//setup
 
@@ -66,24 +84,24 @@ void setup() {
  ************************************
  ************************************/ 
 void loop() {
-  uint8_t i=0;
-  while (Serial.available()>0){ //Listen if promini_reader sends something
-		
-    //Serial.readBytes(data_to_send,300);
-    data_to_send[i]=Serial.read();
-		i++;
-    if(data_to_send[i]=='#'){
-      break;
+
+  while (readerSerial.available()>0){
+    //delay(100);
+    readerSerial.readBytes(data_to_send,512);
+    delay(500);
+    Serial.println(data_to_send); // -> delete / comment
+    //send_data_to_server(data_to_send);
+    /*
+    if(true){ //si se manda exitosamente a la nube
+      ;//vaciar el buffer
     }
+    */
   }
-   /*********************************
-   ****** Send data to server ******
-   *********************************/
-  send_data_to_server(data_to_send);
-	Serial.println(data_to_send);
+
   /********************
    ***** End loop *****
    ********************/
+
 }
 
      
@@ -95,17 +113,28 @@ void loop() {
  *****************************************/
 
 
-int send_data_to_server(String message_to_server){
-  gprs.connectTCP(server, port);/*
-  while(0 != gprs.connectTCP(server, port)){
-      Serial.println(message_to_server);
-      delay(1000);
-  }*/
-  char* tcp_snd = const_cast<char*>(message_to_server.c_str()); //Parse payload to char array
+void send_data_to_server(String message_to_server){
+  //gprs.closeTCP();
   message_to_server.replace("\n","");
   message_to_server.replace("\t","");
   message_to_server.replace("\r","");
-  //gprs.shutTCP();
+  char* tcp_snd = const_cast<char*>(message_to_server.c_str()); //Parse payload to char array
+  Serial.print("msg: ");
+  Serial.println(tcp_snd);
+  if(0 == gprs.connectTCP(server, port)){
+    Serial.print("connect successfuly to: ");
+    Serial.print(server);
+    Serial.print(":");
+    Serial.println(port);
+  }
+  else{
+    Serial.println("connect error");
+    gprs.closeTCP(); 
+    return;
+  }
+  if(0 == gprs.sendTCPData(tcp_snd)){Serial.println("sent");}
+  else{Serial.println("not sent");}
   gprs.closeTCP();
-  return gprs.sendTCPData(tcp_snd);
+  
 }
+
